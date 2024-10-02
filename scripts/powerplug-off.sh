ced00f8fa82a5eeaade4dc1099fa01886d8643f2
pi <pi@vz235-37c7e5d>
2024-10-02 21:09:05 +0200

    New Backup on boot - 02/10/24 - 21:09:02

--- scripts/powerplug-off.sh
+++ scripts/powerplug-off.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# /etc/systemd/system/powerplug.service
+#
+# [Unit]
+# Description=Schedule PowerPlug OFF
+# After=network-online.target
+# Conflicts=reboot.target
+
+# [Service]
+# Type=oneshot
+# ExecStart=/bin/true
+# RemainAfterExit=yes
+# ExecStop=/home/pi/scripts/powerplug-off.sh
+
+# [Install]
+# WantedBy=multi-user.target
+
+
 if /usr/bin/systemctl list-jobs | egrep -q 'reboot.target.*start' ; then
       echo Skip PowerPlug OFF for reboots
 else
--- scripts/powerplug-online.sh
+++ scripts/powerplug-online.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# crontab -e
+# @reboot sleep 60 && /usr/bin/sh /home/pi/scripts/powerplug-online.sh
+
 /usr/bin/curl -s http://iobroker:8087/set/0_userdata.0.Script-Trigger.vz235klip_online?value=true
 
