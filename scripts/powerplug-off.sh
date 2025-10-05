1f1f4bde65c27ed062f33ab3bf27e2d25fffb2ac
pi <pi@vz235-37c7e5d>
2025-10-05 20:27:48 +0200

    powerplug-off.sh modified - 05/10/25 - 20:27:44

--- scripts/powerplug-off.sh
+++ scripts/powerplug-off.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-# /etc/systemd/system/powerplug.service
-#
-# [Unit]
-# Description=Schedule PowerPlug OFF
-# After=network-online.target
-# Conflicts=reboot.target
-
-# [Service]
-# Type=oneshot
-# ExecStart=/bin/true
-# RemainAfterExit=yes
-# ExecStop=/home/pi/scripts/powerplug-off.sh
-
-# [Install]
-# WantedBy=multi-user.target
-
+# schaltet die Dose nach 15 Sekunden aus
 
 if /usr/bin/systemctl list-jobs | egrep -q 'reboot.target.*start' ; then
       echo Skip PowerPlug OFF for reboots
 else
-    /usr/bin/curl -s http://iobroker:8087/set/0_userdata.0.Script-Trigger.vz235klip_online?value=false
+    /usr/bin/curl -s "http://192.168.178.29/cm?cmnd=Backlog%20Delay%20150%3BPower1%20Off"
 fi
